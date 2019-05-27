715fd7865902c95314a4bb17b493afb21cc9f251
Alex Zhang <zhangchi0104@live.com>
2019-05-27 10:21:00 +1000

    Updates

---
+++ precache-manifest.46e5d8e7ca4a9d419b418f24cd187487.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5d3a99c91dad775d0e860550da6419",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "a1d5413aa2a4d91f0e40",
+    "url": "/resume/static/js/2.44e4ee60.chunk.js"
+  },
+  {
+    "revision": "952936de5a76c5a4ea27",
+    "url": "/resume/static/js/main.b0442706.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
